02248f2ef19734d2b01a73bd905f9b0ee52deced
girija7 <girijagandham3669@gmail.com>
2025-06-24 21:40:45 +0530

    Create intent.py

---
+++ intent.py
@@ -0,0 +1,24 @@
+{
+  "intents": [
+    {
+      "tag": "greeting",
+      "patterns": ["Hi", "Hello", "Hey", "Good morning"],
+      "responses": ["Hello!", "Hi there!", "Greetings!"]
+    },
+    {
+      "tag": "goodbye",
+      "patterns": ["Bye", "Goodbye", "See you later"],
+      "responses": ["Goodbye!", "See you soon!", "Have a nice day!"]
+    },
+    {
+      "tag": "thanks",
+      "patterns": ["Thanks", "Thank you", "Appreciate it"],
+      "responses": ["You're welcome!", "Happy to help!", "Anytime!"]
+    },
+    {
+      "tag": "hours",
+      "patterns": ["When are you open?", "What are your hours?", "When do you close?"],
+      "responses": ["We're open 9am-5pm Monday to Friday", "Our hours are 9-5 weekdays"]
+    }
+  ]
+}
